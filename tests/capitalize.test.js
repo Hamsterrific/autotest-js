8f3efd994abd1b0492b8cb08e6f53657e759a08e
Hamsterrific <simbul@gmail.com>
2022-12-16 18:07:36 +0400

    feat: jest

--- tests/capitalize.test.js
+++
@@ -1,6 +0,0 @@
-import { capitalize } from '../src/capitalize.js';
-import { strict as assert } from 'node:assert';
-
-assert.equal(capitalize(''), '');
-assert.equal(capitalize('hello'), 'Hello');
-  console.log('Все тесты пройдены!');
